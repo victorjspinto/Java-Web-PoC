2842420b88738428a32a021d9c90b2ea8b69d70b
Lukax <lucas.espindola@outlook.com>
2013-06-08 00:57:50 -0300

    Adicionado automacao para Typescript no projeto web

---
+++ javawebpoc-html/GruntFile.js
@@ -0,0 +1,40 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: './'
+                }
+            }
+        },
+        typescript: {
+            base: {
+                src: ['lib/**/*.ts'],
+                dest: 'js/PixelVisionJSDemos.js',
+                options: {
+                    module: 'amd',
+                    target: 'es5'
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:8080/index.html'
+            }
+        }
+    });
+ 
+    grunt.registerTask('default', ['connect', 'open', 'watch']);
+ 
+}
