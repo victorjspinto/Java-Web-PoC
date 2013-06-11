dc58d1eaa63e58877d81162846f9115ed7d0ce9a
Victor.Pinto <victor.pinto@dclick.com.br>
2013-06-11 19:32:13 -0300

    refactory de listagem e funcionalidade de warning na tela

--- javawebpoc-html/src/service/mock/StudentServiceMock.ts
+++ javawebpoc-html/src/service/mock/StudentServiceMock.ts
@@ -1,8 +1,14 @@
 ///<reference path='../contract/StudentServiceContract.ts'/>
 ///<reference path='base/AbstractCrudServiceMock.ts'/>
+///<reference path='../../../entity/Student.ts'/>
 
 module service.mock {
     export class StudentServiceMock extends base.AbstractCrudServiceMock implements contract.StudentServiceContract {
-
+        constructor($timeout:ng.ITimeoutService) {
+            super($timeout);
+            this.repo.push(new entity.Student(1, "Victor"));
+            this.repo.push(new entity.Student(2, "Vinicius"));
+            this.repo.push(new entity.Student(3, "Test"));
+        }
     }
 }
--- javawebpoc-html/src/service/mock/base/AbstractCrudServiceMock.ts
+++ javawebpoc-html/src/service/mock/base/AbstractCrudServiceMock.ts
@@ -1,10 +1,17 @@
 ///<reference path='../../contract/base/CrudServiceContract.ts'/>
+///<reference path='../../../../definitionFiles/angular/angular.d.ts'/>
 
 module service.mock.base {
 
     export class AbstractCrudServiceMock implements contract.base.CrudServiceContract {
 
         public repo: any[] = [];
+
+        public timeoutService: ng.ITimeoutService;
+
+        constructor($timeout: ng.ITimeoutService) {
+            this.timeoutService = $timeout;
+        }
 
         save(item: entity.base.BaseEntity, successCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any,
             faultCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any) {
@@ -20,12 +27,14 @@ module service.mock.base {
 
         findById(id: number, successCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any,
             faultCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any) {
+            
         }
 
         all(successCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any,
             faultCallback: (data: any, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any) {
+
+            this.timeoutService(() => successCallback(this.repo, 200, null, null), 3000);
         }
     }
-
 }
 
