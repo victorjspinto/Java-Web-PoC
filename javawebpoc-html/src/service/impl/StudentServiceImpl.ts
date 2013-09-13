e74eb011cd6691d346adf61891ee66f06c0ae213
lordviktor <victorjosesilvapinto@hotmail.com>
2013-09-12 23:22:31 -0300

    Incluido exemplo de como passar pelo processo de minification

--- javawebpoc-html/src/service/impl/StudentServiceImpl.ts
+++ javawebpoc-html/src/service/impl/StudentServiceImpl.ts
@@ -4,6 +4,9 @@
 module service.impl {
 
     export class StudentServiceImpl extends base.AbstractCrudServiceImpl<entity.Student> implements contract.StudentServiceContract {
+
+		static $inject = ['$http'];
+
         constructor($http: ng.IHttpService) {
             super($http, "student");
         }
