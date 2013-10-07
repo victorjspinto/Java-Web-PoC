5b7acff62fcdd8ce31dca45d69240b73402caf24
Lucas Espindola <lucas.espindola@outlook.com>
2013-10-07 01:00:17 -0300

    upgrade angular to 1.2.0-rc.2

--- javawebpoc-html/src/app/main.ts
+++ javawebpoc-html/src/app/main.ts
@@ -4,9 +4,10 @@ require.config({
     baseUrl: '/app',
 
     paths: {
-        'jquery': './../lib/jquery/jquery',
-        'angular': './../lib/angular/angular',
-        'bootstrap': './../lib/bootstrap/dist/js/bootstrap'
+        'jquery': '../lib/jquery/jquery',
+        'angular': '../lib/angular/angular',
+        'angularRoute': '../lib/angular-route/angular-route',
+        'bootstrap': '../lib/bootstrap/dist/js/bootstrap'
     },
 
     shim: {
@@ -14,8 +15,12 @@ require.config({
             exports: '$'
         },
         angular: {
-            exports: 'angular',
-            deps: ['jquery']
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        angularRoute: {
+            deps: ['angular'],
+            exports: 'angularRoute'
         },
         bootstrap: {
             deps: ['jquery']
