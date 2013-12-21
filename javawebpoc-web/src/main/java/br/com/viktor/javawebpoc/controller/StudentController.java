01208b431f56a4cc72d3cb6adcdd185a0a0b097e
Lucas Espindola <lucas.espindola@outlook.com>
2013-12-21 19:08:05 -0200

    included new Grade entity operations in Student and Grade controller

--- javawebpoc-web/src/main/java/br/com/viktor/javawebpoc/controller/StudentController.java
+++ javawebpoc-web/src/main/java/br/com/viktor/javawebpoc/controller/StudentController.java
@@ -1,21 +1,37 @@
 package br.com.viktor.javawebpoc.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.viktor.javawebpoc.controller.base.AbstractCrudController;
+import br.com.viktor.javawebpoc.entity.Grade;
 import br.com.viktor.javawebpoc.entity.Student;
+import br.com.viktor.javawebpoc.exception.invalidArgument.NullArgumentException;
+import br.com.viktor.javawebpoc.exception.notFound.NotFoundException;
 import br.com.viktor.javawebpoc.facade.StudentFacade;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController extends AbstractCrudController<Student>  {
 
+	private StudentFacade studentFacade;
+
 	@Inject
-	public StudentController(StudentFacade facade) {
-		super(facade);
+	public StudentController(StudentFacade studentFacade) {
+		super(studentFacade);
+		this.studentFacade = studentFacade;
 	}
 	
+	@RequestMapping(value = "/{studentId}/grade", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Grade> findAllGradeByStudentId(@PathVariable Long studentId) throws NotFoundException, NullArgumentException{
+		return studentFacade.findAllByStudentId(studentId);
+	}
 }
--- javawebpoc-web/src/main/java/br/com/viktor/javawebpoc/facade/StudentFacade.java
+++ javawebpoc-web/src/main/java/br/com/viktor/javawebpoc/facade/StudentFacade.java
@@ -1,20 +1,40 @@
 package br.com.viktor.javawebpoc.facade;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.viktor.javawebpoc.entity.Grade;
 import br.com.viktor.javawebpoc.entity.Student;
+import br.com.viktor.javawebpoc.exception.invalidArgument.NullArgumentException;
+import br.com.viktor.javawebpoc.exception.notFound.NotFoundException;
 import br.com.viktor.javawebpoc.facade.base.AbstractCrudFacade;
-import br.com.viktor.javawebpoc.service.contract.base.AbstractCrudContract;
+import br.com.viktor.javawebpoc.service.contract.GradeServiceContract;
+import br.com.viktor.javawebpoc.service.contract.StudentServiceContract;
 
 @Component
 @Transactional
 public class StudentFacade extends AbstractCrudFacade<Student> {
 
-	@Autowired
-	public StudentFacade(AbstractCrudContract<Student> abstractCrud) {
-		super(abstractCrud);
+	private StudentServiceContract studentService;
+	private GradeServiceContract gradeService;
+
+	public StudentFacade(){
+		super(null);
+	}
+	
+	@Inject
+	public StudentFacade(StudentServiceContract studentService, GradeServiceContract gradeService) {
+		super(studentService);
+		this.studentService = studentService;
+		this.gradeService = gradeService;
+	}
+	
+	public List<Grade> findAllByStudentId(Long studentId) throws NotFoundException, NullArgumentException{
+		return this.gradeService.findAllByStudent(this.studentService.find(studentId));
 	}
 
 }
