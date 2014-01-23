67eb06a367b5831b9ee2674d1a870e300fcfad7c
lordviktor <victorjosesilvapinto@hotmail.com>
2014-01-23 01:13:14 -0200

    Adicionei suporte ao proxy

--- javawebpoc-html/GruntFile.js
+++ javawebpoc-html/GruntFile.js
@@ -67,13 +67,45 @@ module.exports = function (grunt) {
                 hostname: 'localhost',
                 livereload: 35729
             },
+            proxies: [
+                {
+                    context: '/rest',
+                    host: 'java-web-poc.herokuapp.com',
+                    port: 80,
+                    https: false,
+                    changeOrigin: false,
+                    // xforward: false,
+                    headers: {
+                        "host": "java-web-poc.herokuapp.com"
+                    }
+                }
+            ],
             livereload: {
                 options: {
                     open: true,
                     base: [
                         '.tmp',
                         '<%= yeoman.app %>'
-                    ]
+                    ],
+                    middleware: function (connect, options) {
+                        if (!Array.isArray(options.base)) {
+                            options.base = [options.base];
+                        }
+
+                        // Setup the proxy
+                        var middlewares = [require('grunt-connect-proxy/lib/utils').proxyRequest];
+
+                        // Serve static files.
+                        options.base.forEach(function(base) {
+                            middlewares.push(connect.static(base));
+                        });
+
+                        // Make directory browse-able.
+                        var directory = options.directory || options.base[options.base.length - 1];
+                        middlewares.push(connect.directory(directory));
+
+                        return middlewares;
+                    }
                 }
             },
             test: {
@@ -422,6 +454,7 @@ module.exports = function (grunt) {
         grunt.task.run([
             'clean:server',
             'bower-install',
+            'configureProxies',
             'concurrent:server',
             'autoprefixer',
             'connect:livereload',
