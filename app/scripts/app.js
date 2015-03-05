8d3aa69f60844e502768072ebd36f2446901c37b
Victor Jose Silva Pinto <victorjosesilvapinto@hotmail.com>
2015-03-05 11:10:07 -0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gtplaceFrontendApp
+ * @description
+ * # gtplaceFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gtplaceFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gtplaceFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gtplaceFrontendApp
+ */
+angular.module('gtplaceFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
